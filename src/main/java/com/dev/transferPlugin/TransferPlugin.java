1fe843a009584a8b4e92e6c30f87958c0fa02e47
unknown <xusimone51@gmail.com>
2025-02-27 18:45:30 -0800

    first commit

---
+++ src/main/java/com/dev/transferPlugin/TransferPlugin.java
@@ -0,0 +1,17 @@
+package com.dev.transferPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TransferPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/com/dev/transferPlugin/utils/MessageUtils.java
@@ -0,0 +1,4 @@
+package com.dev.transferPlugin.utils;
+
+public class MessageUtils {
+}
